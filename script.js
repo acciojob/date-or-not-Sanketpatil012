b3196a2d491a64b29d05fe01b2d1541bf81fe622
Sanketpatil012 <122162948+Sanketpatil012@users.noreply.github.com>
2025-03-10 14:18:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 var isDate = function (input) {
-  //   write your code here
+  if (input instanceof Date && !isNaN(input)) {
+        return true;
+    }
+
+    // Check if the input is a string or number and can be converted to a valid date
+    if (typeof input === "string" || typeof input === "number") {
+        const date = new Date(input);
+        return !isNaN(date);
+    }
+
+    return false;
 };
 
 // Do not change the code below.
